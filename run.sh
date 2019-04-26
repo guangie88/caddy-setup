70111a9f36a61646055455a22b18c73a7ac31ddb
Chen Weiguang <chen.weiguang@gmail.com>
2019-04-26 09:48:39 +0800

    Add travis and template

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-BRANCH="1.0.0"
-PLUGINS="filter,"
-
-docker build \
-    -f Dockerfile-no-stats \
-    -t guangie88/caddy:${BRANCH}_filter \
-    --build-arg plugins=${PLUGINS} \
-    github.com/abiosoft/caddy-docker.git#${BRANCH}
